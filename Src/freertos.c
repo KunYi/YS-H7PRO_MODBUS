b37aad7fb166dcf136ab0d6834566537f6824ba5
kunyi chen/陳坤毅 <kunyi.chen@gmail.com>
2022-01-04 20:22:05 +0800

    update sysMytime of settings per second

--- Src/freertos.c
+++ Src/freertos.c
@@ -206,30 +206,32 @@ void basic1SecCallback(void *argument)
   static uint32_t count = 0;
 
   count++;
+  RTC_DateTypeDef sdate={0};
+  RTC_TimeTypeDef stime={0};
+  /*
+    don't change GetTime & GetDate sequence
+    STM32 need keep the operation order for RTC
+  */
+  HAL_RTC_GetTime(&hrtc, &stime, RTC_FORMAT_BIN);
+  HAL_RTC_GetDate(&hrtc, &sdate, RTC_FORMAT_BIN);
+  struct MYTIME temp = {
+                       .year = sdate.Year + 2000,
+                       .month = sdate.Month,
+                       .day = sdate.Date,
+                       .hour = stime.Hours,
+                       .minute = stime.Minutes,
+                       .second = stime.Seconds
+                      };
+
+  uint32_t epoch = datetime_since_epoch(&temp);
+  pSysSettings->myTimeLow = (uint16_t)(epoch & 0xFFFF);
+  pSysSettings->myTimeHigh = (uint16_t)(epoch >> 16);
 
   if ((count % 10) == 0) {
-    RTC_DateTypeDef sdate={0};
-    RTC_TimeTypeDef stime={0};
-    /*
-      don't change GetTime & GetDate sequence
-      STM32 need keep the operation order for RTC
-    */
-    HAL_RTC_GetTime(&hrtc, &stime, RTC_FORMAT_BIN);
-    HAL_RTC_GetDate(&hrtc, &sdate, RTC_FORMAT_BIN);
-    struct MYTIME temp = {
-                           .year = sdate.Year + 2000,
-                           .month = sdate.Month,
-                           .day = sdate.Date,
-                           .hour = stime.Hours,
-                           .minute = stime.Minutes,
-                           .second = stime.Seconds
-                          };
-    uint32_t epoch = datetime_since_epoch(&temp);
-    pSysSettings->myTimeLow = (uint16_t)(epoch & 0xFFFF);
-    pSysSettings->myTimeHigh = (uint16_t)(epoch >> 16);
     SEGGER_RTT_printf(0, "current date:time %0.2d/%0.2d/%0.4d, ", sdate.Month, sdate.Date, sdate.Year + 2000);
     SEGGER_RTT_printf(0, "%0.2d:%0.2d:%0.2d \n", stime.Hours, stime.Minutes, stime.Seconds);
     #if 0
+    SEGGER_RTT_printf(0, "epoch:%d, high16:%d, low16%d\n", epoch, pSysSettings->myTimeHigh, pSysSettings->myTimeLow);
     struct MYTIME mytime={ .year = 2021, .month = 12, .day = 30, .hour = 20, .minute = 1, .second=32 };
     int32_t time =  datetime_since_epoch(&mytime);
     SEGGER_RTT_printf(0, "%d\n", time);
